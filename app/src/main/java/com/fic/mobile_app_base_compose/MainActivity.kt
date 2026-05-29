06cdd341dae24ce93cb9eec92f1a7f0f09a5ab10
Paulrofe <pe.rodriguez23@info.uas.edu.mx>
2026-05-28 21:19:47 -0700

    Main Activity

--- app/src/main/java/com/fic/mobile_app_base_compose/MainActivity.kt
+++ app/src/main/java/com/fic/mobile_app_base_compose/MainActivity.kt
@@ -3,28 +3,19 @@ package com.fic.mobile_app_base_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.fic.mobile_app_base_compose.ui.screens.interfaz_UI
 import com.fic.mobile_app_base_compose.ui.theme.MobileappbasecomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MobileappbasecomposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                interfaz_UI()
             }
         }
     }
